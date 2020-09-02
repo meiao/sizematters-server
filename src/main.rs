adbdf7f5a8b186f9597dd521591148a6e24a710b
Andre Onuki <otanuki@gmail.com>
2020-09-01 21:22:02 -0400

    Adding copyright to all files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * SizeMatters - a ticket sizing util
+ * Copyright (C) 2020 Andre Onuki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 mod actors;
 mod data;
 
@@ -14,10 +32,10 @@ async fn ws_index(
     stream: web::Payload,
     room_manager: web::Data<Addr<RoomManagerActor>>,
 ) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
+    //println!("{:?}", r);
     let room_manager_addr = room_manager.get_ref().clone();
     let res = ws::start(ClientActor::new(room_manager_addr), &r, stream);
-    println!("{:?}", res);
+    //println!("{:?}", res);
     res
 }
 
